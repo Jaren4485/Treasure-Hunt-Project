b0eca87e6cb90277d9f1b3bb56610ac4dd262b67
Jaren4485 <43444293+Jaren4485@users.noreply.github.com>
2019-03-26 09:28:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+ $(".hide-map").hide();
+ $(".hide-chest").hide();
+ $(".hide-text").hide();
+
+$("button").click(function(){
+    $(".hide-map").show();
+    $("body").css("background-color","gold");
+});
